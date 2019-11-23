170135e3b1e626fb034f52f1829c70668a27d42d
Doug <dtolbertcooke@gmail.com>
2019-11-22 22:11:24 -0500

    EmotionForm is now a drop-down menu

--- forms.py
+++ forms.py
@@ -1,23 +1,40 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, IntegerField, SelectField
+from wtforms import StringField, SubmitField, PasswordField, SelectField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 
 class EmotionForm(FlaskForm):
-    emotion1 = StringField('', render_kw={"placeholder": "Enter an emotion..."})
-    emotion2 = StringField('', render_kw={"placeholder": "Enter an emotion..."})
-    emotion3 = StringField('', render_kw={"placeholder": "Enter an emotion..."})
+    emotion1 = SelectField('', choices=[(1, 'Select an emotion...'), ('Fear', 'Fear'),
+                                   ('Anger', 'Anger'), ('Sadness', 'Sadness'),
+                                   ('Joy', 'Joy'), ('Disgust', 'Disgust'),
+                                   ('Surprise', 'Surprise'), ('Trust', 'Trust'),
+                                   ('Anticipation', 'Anticipation')], default=1)
+    emotion2 = SelectField('', choices=[(1, 'Select an emotion...'), ('Fear', 'Fear'),
+                                   ('Anger', 'Anger'), ('Sadness', 'Sadness'),
+                                   ('Joy', 'Joy'), ('Disgust', 'Disgust'),
+                                   ('Surprise', 'Surprise'), ('Trust', 'Trust'),
+                                   ('Anticipation', 'Anticipation')], default=1)
+    emotion3 = SelectField('', choices=[(1, 'Select an emotion...'), ('Fear', 'Fear'),
+                                   ('Anger', 'Anger'), ('Sadness', 'Sadness'),
+                                   ('Joy', 'Joy'), ('Disgust', 'Disgust'),
+                                   ('Surprise', 'Surprise'), ('Trust', 'Trust'),
+                                   ('Anticipation', 'Anticipation')], default=1)
     submit = SubmitField('Submit')
 
 
 class LoginForm(FlaskForm):
-    username = StringField('', render_kw={"placeholder": "Username"}, validators=[DataRequired(), Length(min=3, max=20)])
-    password = PasswordField('', render_kw={"placeholder": "Password"}, validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField('', render_kw={"placeholder": "Username"},
+                           validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField('', render_kw={"placeholder": "Password"},
+                             validators=[DataRequired(), Length(min=3, max=20)])
     login = SubmitField('Login')
 
 
 class RegisterForm(FlaskForm):
-    fName = StringField('', render_kw={"placeholder": "First name"}, validators=[DataRequired(), Length(min=3, max=20)])
-    lName = StringField('', render_kw={"placeholder": "Last name"}, validators=[DataRequired(), Length(min=3, max=20)])
-    username = StringField('', render_kw={"placeholder": "Username"}, validators=[DataRequired(), Length(min=6, max=35)])
+    fName = StringField('', render_kw={"placeholder": "First name"},
+                        validators=[DataRequired(), Length(min=3, max=20)])
+    lName = StringField('', render_kw={"placeholder": "Last name"},
+                        validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField('', render_kw={"placeholder": "Username"},
+                           validators=[DataRequired(), Length(min=6, max=35)])
     submit = SubmitField('Submit')
