da2bf9d58b331fe332e9403d4f1b3886e1a226dc
Doug <dtolbertcooke@gmail.com>
2020-02-04 13:29:28 -0500

    Prototype

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ from bs4 import BeautifulSoup as SOUP
 import re
 import requests as HTTP
 import numpy as np
+
 # from flask_user import roles_required
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Infectious is a fantastic app'
-app.db = None
+# app.db = None
+# app.db = pymysql.connect('127.0.0.1', 'root', 'infectious', 'infectious')
+# c = app.db.cursor()
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -24,59 +27,76 @@ moment = Moment(app)
 def get_name_from_game_id(game_id):
     return df[df.game_id == game_id]["name"].values[0]
 
+# def get_name_from_game_id(game_id):
+#     return c.execute('SELECT name FROM mytable WHERE game_id = %s;' % game_id)
 
 def get_game_id_from_name(name):
     return df[df.name == name]["game_id"].values[0]
 
+# def get_game_id_from_name(name):
+#     return c.execute('SELECT game_id FROM mytable WHERE name = %s;' % name)
 
 def combine_features(row):
     return row['categories'] + " " + row['genres'] + " " + row["steamspy_tags"]
 
 
-df = pd.read_csv("data/steam_games_dataset.csv")
+# df = pd.read_csv("data/steam_games_dataset.csv")
 # print(df.columns)
+df = pd.read_csv("data/small_dataset.csv")
 
 
-def get_categories():
-    app.db = pymysql.connect('127.0.0.1', 'root', 'infectious', 'infectious')
-    c = app.db.cursor()
-    c.execute('SELECT categories FROM mytable;')
-    categories_data = c.fetchall()
-    app.db.commit()
-    c.close()
-    return categories_data
+# def get_categories():
+#     # app.db = pymysql.connect('127.0.0.1', 'root', 'infectious', 'infectious')
+#     # c = app.db.cursor()
+#     c.execute('SELECT categories FROM mytable;')
+#     categories_data = c.fetchall()
+#     app.db.commit()
+#     c.close()
+#     return categories_data
+#
+#
+# def get_genres():
+#     c = app.db.cursor()
+#     query = "SELECT genres FROM mytable;"
+#     c.execute(query)
+#     genres_data = c.fetchall()
+#     app.db.commit()
+#     c.close()
+#     return genres_data
+#
+#
+# def get_steamspy_tags():
+#     query = "SELECT genres FROM mytable;"
+#     c = app.db.cursor()
+#     c.execute(query)
+#     steamspy_data = c.fetchall()
+#     app.db.commit()
+#     c.close()
+#     return steamspy_data
+#
+#
+# def combine():
+#     categories = get_categories()
+#     genres = get_genres()
+#     steamspy_id = get_steamspy_tags()
+#     combined = categories + genres + steamspy_id
+#     return combined
 
 
-def get_genres():
-    c = app.db.cursor()
-    query = "SELECT genres FROM mytable;"
-    c.execute(query)
-    genres_data = c.fetchall()
-    app.db.commit()
-    c.close()
-    return genres_data
-
-
-def get_steamspy_tags():
-    query = "SELECT genres FROM mytable;"
-    c = app.db.cursor()
-    c.execute(query)
-    steamspy_data = c.fetchall()
-    app.db.commit()
-    c.close()
-    return steamspy_data
-
+# print(combine())
 
 # categories = get_categories()
 # genres = get_genres()
 # steamspy_id = get_steamspy_tags()
-#
+
 # print(categories[0], genres[0], steamspy_id[0])
 
 features = ['categories', 'genres', 'steamspy_tags']
 df["combined_features"] = df.apply(combine_features, axis=1)
-cv = CountVectorizer() # Convert a collection of text documents to a matrix of token counts
+cv = CountVectorizer()  # Convert a collection of text documents to a matrix of token counts
 count_matrix = cv.fit_transform(df["combined_features"])
+# count_matrix = cv.fit_transform(combine())
+# print(type(df["combined_features"])) // --> <class 'pandas.core.series.Series'>
 cosine_sim = cosine_similarity(count_matrix)
 # game_user_likes = "Left 4 Dead"
 
@@ -96,11 +116,11 @@ def home():
 
         i = 0
         for element in sorted_similar_games:
-            # similar_games_array.append(get_name_from_game_id(element[0]))
+            similar_games_array.append(get_name_from_game_id(element[0]))
             # print(similar_games_array[i])
             sim_games = get_name_from_game_id(element[0])
             similar_games_array.append(sim_games)
-            #print(sim_games)
+            # print(sim_games)
             i = i + 1
             if i >= 7:
                 break
@@ -121,7 +141,8 @@ def results():
     game4 = similar_games_array[4]
     game5 = similar_games_array[5]
     game6 = similar_games_array[6]
-    return render_template("results.html", title=title, game1=game1, game2=game2, game3=game3, game4=game4, game5=game5, game6=game6)
+    return render_template("results.html", title=title, game1=game1, game2=game2, game3=game3, game4=game4, game5=game5,
+                           game6=game6)
 
 
 @app.route('/register', methods=['GET', 'POST'])
